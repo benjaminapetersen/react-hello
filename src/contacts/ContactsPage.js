761e164a0ac77b6e1fc1b98ffca8c577ed4f5ec3
benjaminapetersen <admin@benjaminapetersen.me>
2017-07-31 23:02:38 -0400

    Tweak contacts

---
+++ src/contacts/ContactsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ContactsPage extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <p>Page: <strong>Contacts</strong></p>
+        <p>(The contacts stuff should go here...)</p>
+      </div>
+    )
+  }
+}
